d89ccac9a8d58bd2b161fce16767a54d26a31170
kalibrahim <bowaleed500@gmail.com>
2019-12-04 09:41:35 +0300

    Comp: Icons added to nav bar

--- frontend/client/src/components/navigation/navigation.js
+++ frontend/client/src/components/navigation/navigation.js
@@ -1,50 +1,104 @@
 import React from 'react';
 import './navigation.css';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStroopwafel, faHome, faConciergeBell, faBell, faAddressBook, faQuestionCircle, faLink, faUserCircle, faPlane } from "@fortawesome/free-solid-svg-icons";
+
+
+library.add(faStroopwafel);
 
 export default class Navigation extends React.Component {
     render() {
         return (
-    // <script type="text/javascript">
-    //     {/* // Sets active link in Bootstrap menu
-    //     // Add this code in a central place used\shared by all pages
-    //     // like your _Layout.cshtml in ASP.NET MVC for example */}
-    //     $('a[href="' + this.location.pathname + '"]').parents('li,ul').addClass('active');
-    // </script>
-    <div class="wrapper">
-        {/* Sidebar */}
-        <nav id="sidebar">
-            <ul class="list-unstyled components">
+          // <script type="text/javascript">
+          //     {/* // Sets active link in Bootstrap menu
+          //     // Add this code in a central place used\shared by all pages
+          //     // like your _Layout.cshtml in ASP.NET MVC for example */}
+          //     $('a[href="' + this.location.pathname + '"]').parents('li,ul').addClass('active');
+          // </script>
+          <div className="wrapper">
+            {/* Sidebar */}
+            <nav id="sidebar">
+              <ul className="list-unstyled components">
                 <li id="home">
-                    <a href="#" data-toggle="tooltip" data-placement="right" title="Home"><span class="fa fa-home"
-                            aria-hidden="true"></span></a>
+                  <a
+                    href="#"
+                    data-toggle="tooltip"
+                    data-placement="right"
+                    title="Home">
+                    <FontAwesomeIcon
+                      icon={faHome}
+                      aria-hidden="true"
+                      size="sm"
+                    />
+                  </a>
                 </li>
                 <li id="notification">
-                    <a href="#" data-toggle="tooltip" data-placement="right" title="Notifications"><span
-                            class="fas fa-bell" aria-hidden="true"></span></a>
+                  <a
+                    href="#"
+                    data-toggle="tooltip"
+                    data-placement="right"
+                    title="Notifications">
+                    <FontAwesomeIcon icon={faBell} aria-hidden="true" size="sm" />
+                  </a>
                 </li>
                 <li id="contact">
-                    <a href="#" data-toggle="tooltip" data-placement="right" title="Contacts"><span
-                            class="fas fa-address-book" aria-hidden="true"></span></a>
+                  <a
+                    href="#"
+                    data-toggle="tooltip"
+                    data-placement="right"
+                    title="Contacts">
+                    <FontAwesomeIcon
+                      icon={faAddressBook}
+                      aria-hidden="true" 
+                      size="sm" />
+                  </a>
                 </li>
                 <li id="help">
-                    <a href="#" data-toggle="tooltip" data-placement="right" title="Help"><span
-                            class="fas fa-question-circle" aria-hidden="true"></span></a>
+                  <a
+                    href="#"
+                    data-toggle="tooltip"
+                    data-placement="right"
+                    title="Help">
+                    <FontAwesomeIcon
+                      icon={faQuestionCircle}
+                      aria-hidden="true" 
+                      size="sm"/>
+                  </a>
                 </li>
                 <li id="link">
-                    <a href="#" data-toggle="tooltip" data-placement="right" title="Links"><span class="fas fa-link"
-                            aria-hidden="true"></span></a>
+                  <a
+                    href="#"
+                    data-toggle="tooltip"
+                    data-placement="right"
+                    title="Links">
+                    <FontAwesomeIcon icon={faLink} aria-hidden="true" size="sm" />
+                  </a>
                 </li>
-                <li class="footer" id="profile">
-                    <a href="#" data-toggle="tooltip" data-placement="right" title="Profile"><span
-                            class="fas fa-user-circle" aria-hidden="true"></span></a>
+                <li className="footer" id="profile">
+                  <a
+                    href="#"
+                    data-toggle="tooltip"
+                    data-placement="right"
+                    title="Profile">
+                    <FontAwesomeIcon
+                      icon={faUserCircle}
+                      aria-hidden="true" 
+                      size="sm" />
+                  </a>
                 </li>
                 <li id="qr">
-                    <a href="#" data-toggle="tooltip" data-placement="right" title="QR"><span class="fas fa-plane"
-                            aria-hidden="true"></span></a>
+                  <a
+                    href="#"
+                    data-toggle="tooltip"
+                    data-placement="right"
+                    title="QR">
+                    <FontAwesomeIcon icon={faPlane} aria-hidden="true" size="sm"/>
+                  </a>
                 </li>
-            </ul>
-        </nav>
-        </div>
-        )
+              </ul>
+            </nav>
+          </div>
+        );
     }
 }
