d5027f3c2bbdbf7770ef89ba4aa1f276de7cccb8
kalibrahim <bowaleed500@gmail.com>
2019-12-29 12:45:35 +0300

    Comp: Table sorting is functional

--- frontend/client/src/components/table/PMTable.js
+++ frontend/client/src/components/table/PMTable.js
@@ -3,6 +3,7 @@ import 'antd/dist/antd.css';
 import './PMTable.css';
 import PMFilter from './filters';
 import { Table, Tag, Icon, Avatar, Row, Col, Select, Button } from 'antd';
+import moment from 'moment';
 
 export default class PMTable extends React.Component {
   constructor(props) {
@@ -37,7 +38,7 @@ export default class PMTable extends React.Component {
       },
       {
         key: '3',
-        name: 'Fathima',
+        name: 'Fatima',
         id: 49591,
         stream: ['COM (Delegated)'],
         phase: ['Familiarization'],
@@ -162,6 +163,16 @@ export default class PMTable extends React.Component {
     }
   }
 
+  compareByAlpha(a, b) {
+    if (a > b) {
+      return -1;
+    }
+    if (a < b) {
+      return 1;
+    }
+    return 0;
+   }
+
   clearFilters() {
     this.setState({
       buttonD: true,
@@ -174,38 +185,43 @@ export default class PMTable extends React.Component {
   render() {
     const columns = [
       {
-        dataIndex: 'avatar',
-        key: 'avatar',
+        dataIndex: "avatar",
+        key: "avatar",
         width: 64,
         render: icon => <Avatar size="default" icon="user" />
       },
       {
-        title: 'Name',
-        dataIndex: 'name',
-        key: 'name',
-        sorter: true
+        title: "Name",
+        dataIndex: "name",
+        key: "name",
+        sorter: (a, b) => this.compareByAlpha(a.name, b.name),
+        // sortDirection: ["descend", "ascend"]
       },
       {
-        title: 'Staff ID',
-        dataIndex: 'id',
-        key: 'id'
+        title: "Staff ID",
+        dataIndex: "id",
+        key: "id",
+        sorter: (a, b) => a.id - b.id,
+        // sortDirection: ["descend", "ascend"]
       },
       {
-        title: 'Stream',
-        key: 'stream',
-        dataIndex: 'stream',
+        title: "Stream",
+        key: "stream",
+        dataIndex: "stream",
+        sorter: (a, b) => this.compareByAlpha(a.stream, b.stream),
+        // sortDirection: ["descend", "ascend"],
         render: tags => (
           <span>
             {tags.map(tag => {
               var color;
-              if (tag === 'IT') {
-                color = 'rgba(0, 0, 0, 0.25)';
+              if (tag === "IT") {
+                color = "rgba(0, 0, 0, 0.25)";
               }
-              if (tag === 'HR') {
-                color = 'rgba(0, 80, 0, 0.25)';
+              if (tag === "HR") {
+                color = "rgba(0, 80, 0, 0.25)";
               }
-              if (tag === 'COM (Delegated)') {
-                color = 'rgba(170, 170, 0, 0.25)';
+              if (tag === "COM (Delegated)") {
+                color = "rgba(170, 170, 0, 0.25)";
               }
               return (
                 <Tag color={color} key={tag}>
@@ -217,24 +233,26 @@ export default class PMTable extends React.Component {
         )
       },
       {
-        title: 'Phase',
-        key: 'phase',
-        dataIndex: 'phase',
+        title: "Phase",
+        key: "phase",
+        dataIndex: "phase",
+        sorter: (a, b) => this.compareByAlpha(a.phase, b.phase),
+        // sortDirection: ["descend", "ascend"],
         render: tags => (
           <span>
             {tags.map(tag => {
               var color;
-              if (tag === 'OJT') {
-                color = 'rgba(220,220,220, 0.25)';
+              if (tag === "OJT") {
+                color = "rgba(220,220,220, 0.25)";
               }
-              if (tag === 'Familiarization') {
-                color = 'rgba(255,200,0, 0.25)';
+              if (tag === "Familiarization") {
+                color = "rgba(255,200,0, 0.25)";
               }
-              if (tag === 'Rotation') {
-                color = 'rgba(220,220,220, 0.25)';
+              if (tag === "Rotation") {
+                color = "rgba(220,220,220, 0.25)";
               }
-              if (tag === 'Finishing OJT') {
-                color = 'rgba(220,220,220, 0.25)';
+              if (tag === "Finishing OJT") {
+                color = "rgba(220,220,220, 0.25)";
               }
               return (
                 <Tag color={color} key={tag}>
@@ -246,20 +264,23 @@ export default class PMTable extends React.Component {
         )
       },
       {
-        title: 'DOJ',
-        dataIndex: 'doj',
-        key: 'doj'
+        title: "DOJ",
+        dataIndex: "doj",
+        key: "doj",
+        sorter: (a, b) => moment(a.doj).unix() - moment(b.doj).unix()
       },
       {
-        title: 'Alerts',
-        dataIndex: 'alerts',
-        key: 'alerts',
+        title: "Alerts",
+        dataIndex: "alerts",
+        key: "alerts",
+        sorter: (a, b) => this.compareByAlpha(a.alerts, b.alerts),
+        sortDirection: ["descend"],
         render: text => (
           <span>
             {text.length > 0 && (
               <span>
                 <Icon type="info-circle" />
-                <a>{' ' + text}</a>
+                <a>{" " + text}</a>
               </span>
             )}
           </span>
