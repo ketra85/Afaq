cf5547681f8cc5e20080a3fd7a2823f72551c2de
Yousuf Alemadi <yousufalemadi@gmail.com>
2019-12-29 09:46:09 +0300

    comp: filter functionality

--- frontend/client/src/components/table/PMTable.js
+++ frontend/client/src/components/table/PMTable.js
@@ -2,15 +2,175 @@ import React from 'react';
 import 'antd/dist/antd.css';
 import './PMTable.css';
 import PMFilter from './filters';
-import { Table, Tag, Icon, Avatar, Row, Col } from 'antd';
+import { Table, Tag, Icon, Avatar, Row, Col, Select, Button } from 'antd';
 
 export default class PMTable extends React.Component {
   constructor(props) {
     super(props);
+    this.data = [
+      {
+        key: '0',
+        name: 'Eman',
+        id: 76131,
+        stream: ['IT'],
+        phase: ['OJT'],
+        doj: '21-JUN-2018',
+        alerts: 'Missing CV / Area of Interest'
+      },
+      {
+        key: '1',
+        name: 'Khalid',
+        id: 96294,
+        stream: ['HR'],
+        phase: ['OJT'],
+        doj: '06-MAY-2018',
+        alerts: 'Missing Rotation Plan'
+      },
+      {
+        key: '2',
+        name: 'Omar',
+        id: 82746,
+        stream: ['HR'],
+        phase: ['Familiarization'],
+        doj: '09-APR-2018',
+        alerts: 'Missing Rotation Evaluation'
+      },
+      {
+        key: '3',
+        name: 'Fathima',
+        id: 49591,
+        stream: ['COM (Delegated)'],
+        phase: ['Familiarization'],
+        doj: '22-JUL-2018',
+        alerts: 'Missing Rotation Evaluation'
+      },
+      {
+        key: '4',
+        name: 'Maryam',
+        id: 63840,
+        stream: ['COM (Delegated)'],
+        phase: ['Rotation'],
+        doj: '05-JAN-2018',
+        alerts: 'Missing Rotation Plan'
+      },
+      {
+        key: '5',
+        name: 'Yousuf',
+        id: 97789,
+        stream: ['IT'],
+        phase: ['Rotation'],
+        doj: '29-FEB-2018',
+        alerts: 'Missing Rotation Plan'
+      },
+      {
+        key: '6',
+        name: 'Reem',
+        id: 34567,
+        stream: ['HR'],
+        phase: ['Finishing OJT'],
+        doj: '08-OCT-2018',
+        alerts: ''
+      },
+      {
+        key: '7',
+        name: 'Elham',
+        id: 81239,
+        stream: ['IT'],
+        phase: ['Finishing OJT'],
+        doj: '08-OCT-2018',
+        alerts: ''
+      },
+      {
+        key: '8',
+        name: 'Abdulaziz',
+        id: 109283,
+        stream: ['IT'],
+        phase: ['Finishing OJT'],
+        doj: '21-DEC-2018',
+        alerts: ''
+      }
+    ];
     this.state = {
-      tabelData: ''
+      streamApplied: false,
+      phaseApplied: false,
+      buttonD: true,
+      tableData: this.data
     };
+    this.handleStreamChange = this.handleStreamChange.bind(this);
+    this.handlePhaseChange = this.handlePhaseChange.bind(this);
+    this.clearFilters = this.clearFilters.bind(this);
   }
+
+  handleStreamChange(value) {
+    if (value.length === 0) {
+      if (!this.state.phaseApplied) {
+        this.setState({
+          buttonD: true,
+          tableData: this.data,
+          streamApplied: false
+        });
+      } else {
+        this.setState({
+          tableData: this.data,
+          streamApplied: false
+        });
+      }
+    } else {
+      if (!this.state.phaseApplied) {
+        var array = this.data;
+      } else {
+        var array = this.state.tableData;
+      }
+      var filter = [];
+      value.map((tag, index) => {
+        array.map(item => {
+          item.stream[0] === value[index] && filter.push(item);
+        });
+      });
+      this.setState({
+        buttonD: false,
+        tableData: filter,
+        streamApplied: true
+      });
+    }
+  }
+
+  handlePhaseChange(value) {
+    if (value.length === 0) {
+      this.setState({
+        buttonD: true,
+        tableData: this.data,
+        phaseApplied: false
+      });
+    } else {
+      if (!this.state.streamApplied) {
+        var array = this.data;
+      } else {
+        var array = this.state.tableData;
+      }
+      var filter = [];
+      value.map((tag, index) => {
+        array.map(item => {
+          item.phase[0] === value[index] && filter.push(item);
+        });
+      });
+      this.setState({
+        buttonD: false,
+        tableData: filter,
+        phaseApplied: true
+      });
+    }
+  }
+
+  clearFilters() {
+    this.setState({
+      buttonD: true,
+      tableData: this.data,
+      phaseApplied: false,
+      streamApplied: false
+    });
+  }
+
   render() {
     const columns = [
       {
@@ -42,10 +202,10 @@ export default class PMTable extends React.Component {
                 color = 'rgba(0, 0, 0, 0.25)';
               }
               if (tag === 'HR') {
-                color = 'gold';
+                color = 'rgba(0, 80, 0, 0.25)';
               }
               if (tag === 'COM (Delegated)') {
-                color = 'geekblue';
+                color = 'rgba(170, 170, 0, 0.25)';
               }
               return (
                 <Tag color={color} key={tag}>
@@ -65,16 +225,16 @@ export default class PMTable extends React.Component {
             {tags.map(tag => {
               var color;
               if (tag === 'OJT') {
-                color = 'red';
+                color = 'rgba(220,220,220, 0.25)';
               }
               if (tag === 'Familiarization') {
-                color = 'lime';
+                color = 'rgba(255,200,0, 0.25)';
               }
               if (tag === 'Rotation') {
-                color = 'purple';
+                color = 'rgba(220,220,220, 0.25)';
               }
               if (tag === 'Finishing OJT') {
-                color = 'volcano';
+                color = 'rgba(220,220,220, 0.25)';
               }
               return (
                 <Tag color={color} key={tag}>
@@ -107,89 +267,6 @@ export default class PMTable extends React.Component {
       }
     ];
 
-    const data = [
-      {
-        key: '1',
-        name: 'Eman',
-        id: 76131,
-        stream: ['IT'],
-        phase: ['OJT'],
-        doj: '21-JUN-2018',
-        alerts: 'Missing CV / Area of Interest'
-      },
-      {
-        key: '2',
-        name: 'Khalid',
-        id: 96294,
-        stream: ['HR'],
-        phase: ['OJT'],
-        doj: '06-MAY-2018',
-        alerts: 'Missing Rotation Plan'
-      },
-      {
-        key: '3',
-        name: 'Omar',
-        id: 82746,
-        stream: ['HR'],
-        phase: ['Familiarization'],
-        doj: '09-APR-2018',
-        alerts: 'Missing Rotation Evaluation'
-      },
-      {
-        key: '4',
-        name: 'Fathima',
-        id: 49591,
-        stream: ['COM (Delegated)'],
-        phase: ['Familiarization'],
-        doj: '22-JUL-2018',
-        alerts: 'Missing Rotation Evaluation'
-      },
-      {
-        key: '5',
-        name: 'Maryam',
-        id: 63840,
-        stream: ['COM (Delegated)'],
-        phase: ['Rotation'],
-        doj: '05-JAN-2018',
-        alerts: 'Missing Rotation Plan'
-      },
-      {
-        key: '6',
-        name: 'Yousuf',
-        id: 97789,
-        stream: ['IT'],
-        phase: ['Rotation'],
-        doj: '29-FEB-2018',
-        alerts: 'Missing Rotation Plan'
-      },
-      {
-        key: '7',
-        name: 'Reem',
-        id: 34567,
-        stream: ['HR'],
-        phase: ['Finishing OJT'],
-        doj: '08-OCT-2018',
-        alerts: ''
-      },
-      {
-        key: '8',
-        name: 'Elham',
-        id: 81239,
-        stream: ['IT'],
-        phase: ['Finishing OJT'],
-        doj: '08-OCT-2018',
-        alerts: ''
-      },
-      {
-        key: '9',
-        name: 'Abdulaziz',
-        id: 109283,
-        stream: ['IT'],
-        phase: ['Finishing OJT'],
-        doj: '21-DEC-2018',
-        alerts: ''
-      }
-    ];
     const streams = [
       {
         key: 'IT',
@@ -202,8 +279,8 @@ export default class PMTable extends React.Component {
         color: 'rgba(0,80,0,0.25)'
       },
       {
-        key: 'COM',
-        value: 'COM',
+        key: 'COM (Delegated)',
+        value: 'COM (Delegated)',
         color: 'rgba(170,170,0,0.25)'
       }
     ];
@@ -215,8 +292,8 @@ export default class PMTable extends React.Component {
         color: 'rgba(220,220,220,0.25)'
       },
       {
-        key: 'Fimiliarization',
-        value: 'Fimiliarization',
+        key: 'Familiarization',
+        value: 'Familiarization',
         color: 'rgba(255,200,0,0.25)'
       },
       {
@@ -230,12 +307,58 @@ export default class PMTable extends React.Component {
         color: 'rgba(220,220,220,0.25)'
       }
     ];
-    // this.setState({ tabelData: data });
+
     return (
       <div>
         <Row>
           <Col className="headerBox">
-            <PMFilter stream={streams} phase={phases} />
+            <Row type="flex" align="middle" style={{ height: '14vh' }}>
+              <Col offset={2} span={2}>
+                <Icon type="filter" />
+                Stream
+              </Col>
+              <Col span={5}>
+                <Select
+                  mode="tags"
+                  placeholder="Filter Streams"
+                  style={{ width: '100%' }}
+                  onChange={this.handleStreamChange}
+                >
+                  {streams.map(item => (
+                    <Option key={item.key} value={item.value}>
+                      <div>{item.value}</div>
+                    </Option>
+                  ))}
+                </Select>
+              </Col>
+              <Col offset={2} span={2}>
+                <Icon type="filter" />
+                Phase
+              </Col>
+              <Col span={5}>
+                <Select
+                  mode="tags"
+                  placeholder="Filter Phases"
+                  style={{ width: '100%' }}
+                  onChange={this.handlePhaseChange}
+                >
+                  {phases.map(item => (
+                    <Option key={item.key} value={item.value}>
+                      <div>{item.value}</div>
+                    </Option>
+                  ))}
+                </Select>
+              </Col>
+              <Col offset={2} span={2}>
+                <Button
+                  type="link"
+                  disabled={this.state.buttonD}
+                  onClick={this.clearFilters}
+                >
+                  Clear Filters
+                </Button>
+              </Col>
+            </Row>
           </Col>
         </Row>
         <Row>
@@ -244,7 +367,7 @@ export default class PMTable extends React.Component {
               <div className="h5 afaqHeading">Graduate Developees</div>
               <Table
                 columns={columns}
-                dataSource={data}
+                dataSource={this.state.tableData}
                 pagination={{ position: 'false' }}
                 scroll={{ y: 400 }}
               />
--- frontend/client/src/components/table/filters.js
+++ frontend/client/src/components/table/filters.js
@@ -14,53 +14,10 @@ export default class PMTable extends React.Component {
 
   handleChange(value) {
     console.log(value); // { key: "lucy", label: "Lucy (101)" }
-    this.setState({ buttonD: false });
+    this.setState({ buttonD: false, applied: value });
   }
 
   render() {
-    return (
-      <Row type="flex" align="middle" style={{ height: '14vh' }}>
-        <Col offset={2} span={2}>
-          <Icon type="filter" />
-          Stream
-        </Col>
-        <Col span={5}>
-          <Select
-            mode="tags"
-            placeholder="Filter Streams"
-            style={{ width: '100%' }}
-            onChange={this.handleChange}
-          >
-            {this.props.stream.map(item => (
-              <Option key={item.key} value={item.value}>
-                <div>{item.value}</div>
-              </Option>
-            ))}
-          </Select>
-        </Col>
-        <Col offset={2} span={2}>
-          <Icon type="filter" />
-          Phase
-        </Col>
-        <Col span={5}>
-          <Select
-            mode="tags"
-            placeholder="Filter Phases"
-            style={{ width: '100%' }}
-          >
-            {this.props.phase.map(item => (
-              <Option key={item.key} value={item.value}>
-                <div>{item.value}</div>
-              </Option>
-            ))}
-          </Select>
-        </Col>
-        <Col offset={2} span={2}>
-          <Button type="link" disabled={this.state.buttonD}>
-            Clear Filters
-          </Button>
-        </Col>
-      </Row>
-    );
+    return <div></div>;
   }
 }
