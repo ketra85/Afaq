f5befee8ab6b7b7f39d940c08e4c6ebff77cca3f
kalibrahim <bowaleed500@gmail.com>
2020-01-02 12:54:24 +0300

    Pkg: React router

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 import Navigation from './components/navigation/navigation';
 import PMTable from './components/table/PMTable';
